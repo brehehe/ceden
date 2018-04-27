0389b39ef908bbbbdf84aa8c5ac42a99329b34ff
brehehe <36395200+brehehe@users.noreply.github.com>
2018-04-27 17:36:49 +0700

    Update locutus.js

--- js/locutus.js
+++ js/locutus.js
@@ -1,3 +1,6 @@
+var __monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni",
+			"Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+
 function phptime () {
   //  discuss at: http://locutus.io/php/time/
   // original by: GeekFG (http://geekfg.blogspot.com)
