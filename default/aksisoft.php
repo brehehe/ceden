0a75e86dcf3530822a4677e01482c96ff404d0f4
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2023-05-20 16:51:01 +0700

    Create aksisoft.php

---
+++ default/aksisoft.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Aksisoft
+Description: Aksisoft IS3
+Author: RISASONGKO
+Version: 0.0.1
+Author URI: https://aksisoft.com/
+*/
+
+require_once '/var/www/html/filehost/wp.php';
